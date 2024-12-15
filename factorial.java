a32b3a8c2823cc67a30027e678d53ae3667a4792
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-15 10:22:33 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -4,7 +4,7 @@ public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int  factorial = sc.nextInt();
-        System.out.println("Amount after 10 percent discount is - ");
+        System.out.println("Amount after 10 percent discount is- ");
        int  discount = (factorial * 10) / 100;
        int discountprize = factorial - discount;
        System.out.println(discountprize);
